bcc5638778f1bb93c5a07baca325ba722360a24a
Yamkela Jack <yamkelajack06@gmail.com>
2026-05-03 16:32:46 +0200

    update

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', 0);
+error_reporting(0);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
